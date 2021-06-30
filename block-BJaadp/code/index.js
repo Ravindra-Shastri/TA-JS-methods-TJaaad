f1ab9a0beb79c477b742fd288483d483c6343e38
Ravindra Shastri <ravindrashastri90@gmail.com>
2021-06-30 23:24:35 +0530

    practice array and string methods

--- block-BJaadp/code/index.js
+++ block-BJaadp/code/index.js
@@ -16,11 +16,11 @@ let persons = [
 // NOTE: Use reduce method whereever you can to solve this exercise:
 
 // Find the average grade
-
+console.log
 // Find the average grade of male
 
 // Find the average grade of female
-
+console.log
 // Find the highest grade
 
 // Find the highest grade in male
